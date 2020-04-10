83297a90b6a062c5dd26a20a3344f3d87dcc9f27
Mazinho Zanelato <mazinhovz@gmail.com>
2020-04-10 19:42:44 -0400

    add ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React from "react";
 
 import "./styles.css";
+import { useState, useEffect } from "react";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(() => {
+    api.get('/repositories').then (response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('/repositories', {
+      title: `Novo Repositório ${Date.now()}`,
+      url: "www.teste.com",
+      techs: "Node, JS, Vara de goiaba"
+    });
+    const repository = response.data;
+    setRepositories([... repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    const response = await api.delete('/repositories/:id');
+    const repository = response.data;
+    setRepositories([... repositories, repository]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
+        {
+          repositories.map(repository => 
+          <li key={ repository.id }>
+            { repository.title }
+          </li>)
+        }
         <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repositories.id)}>
             Remover
           </button>
         </li>
